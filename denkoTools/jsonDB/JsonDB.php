37b41ef05b932923af21eecb4b8d500ecbdb63a6
Federico Bricker <fbricker@dokkogroup.com.ar>
2018-01-30 20:33:11 -0300

    add getField_dbname__fieldname functions

--- denkoTools/jsonDB/JsonDB.php
+++ denkoTools/jsonDB/JsonDB.php
@@ -39,16 +39,32 @@ class JsonDB {
 
 	///////////////////////////////////////////////////////////////////////////
 
+	public static function getOneFieldFromDB($dbName,$field) {
+		$key = $dbName.'->'.$field;
+		if(isset(self::$CACHE_JSONDB[$key])) return self::$CACHE_JSONDB[$key];
+		$res = array();
+		foreach(self::getJsonDB($dbName) as $k => $v) {
+			if(isset($v[$field])) $res[$k] = $v[$field];
+		}
+		self::$CACHE_JSONDB[$key] = $res;
+		return $res;
+	}
+
+	///////////////////////////////////////////////////////////////////////////
+
 	public static function __callStatic($name, $arguments) {
 		$aux = explode('_',$name,2);
-		if(count($aux)!=2 || $aux[0]!='getFull') {
+		if(count($aux)!=2 || ($aux[0]!='getFull' && $aux[0]!='getField')) {
 			throw new Exception("Call to unkown function JsonDB::$name", 1);
 		}
 		if(count($arguments)>0) {
 			throw new Exception("JsonDB::$name requires no arguments", 1);
 		}
-		return self::getJsonDB($aux[1]);
-    }
+		if($aux[0]=='getFull') return self::getJsonDB($aux[1]);
+		$aux = explode('__',$aux[1],2);
+		if(count($aux)!=2) throw new Exception("Call to unkown function JsonDB::$name", 1);
+		return self::getOneFieldFromDB($aux[0],$aux[1]);
+	}
 
 	///////////////////////////////////////////////////////////////////////////
 
